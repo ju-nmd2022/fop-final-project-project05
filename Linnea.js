fc0d38c39188472193e3f4500c23945c141429ed
kuli22te <kuli22te@student.ju.se>
2023-04-18 14:56:59 +0200

    2023-04-18
    Started drawing Baby monster

---
+++ Linnea.js
@@ -0,0 +1,26 @@
+//body
+fill(238, 161, 221);
+stroke(177, 78, 155);
+strokeWeight(5);
+ellipse(200, 300, 150);
+
+//eye
+fill(24, 24, 24);
+noStroke();
+ellipse(200, 295, 60);
+
+fill(255, 255, 255);
+noStroke();
+ellipse(210, 285, 20);
+
+fill(255, 255, 255);
+noStroke();
+ellipse(190, 303, 25);
+
+//blush
+
+fill(177, 78, 155);
+ellipse(158, 329, 20, 10);
+
+fill(177, 78, 155);
+ellipse(240, 329, 20, 10);
