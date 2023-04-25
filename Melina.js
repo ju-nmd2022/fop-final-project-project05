1f6a13ea1b703f3799d8777d32ef4161c1b6ea17
mera22qs <118453987+mera22qs@users.noreply.github.com>
2023-04-25 17:23:31 +0200

    Update Melina.js

---
+++ Melina.js
@@ -0,0 +1,10 @@
+background(255, 255, 255);
+// Monster Guide Layout
+fill(152, 218, 214);
+noStroke();
+ellipse(160, 166, 15, 40);
+ellipse(240, 166, 15, 40);
+stroke(90, 110, 129);
+strokeWeight(4);
+ellipse(200, 200, 100);
+line(170, 180, 140);
