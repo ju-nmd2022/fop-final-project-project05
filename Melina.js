dcb1716d57a96ee77c5a6521b3c10d34fc01cc15
mera22qs <118453987+mera22qs@users.noreply.github.com>
2023-04-27 12:45:42 +0200

    characters

--- Melina.js
+++ Melina.js
@@ -1,38 +1,66 @@
 background(255, 255, 255);
 // Monster Guide Layout
+
+// eyebrows
 stroke(152, 218, 214);
 strokeWeight(5);
 line(185, 230, 185, 300);
 line(225, 230, 225, 300);
+
+// feet
 fill(152, 218, 214);
 ellipse(170, 299, 30, 10);
 ellipse(210, 299, 30, 10);
+
+// ear
 noStroke();
 ellipse(160, 166, 15, 40);
 ellipse(240, 166, 15, 40);
+
+// head/body
 stroke(90, 110, 129);
 strokeWeight(4);
 fill(152, 218, 214);
 ellipse(200, 200, 100);
+
+// eyes
 fill(255, 255, 250);
 noStroke();
 ellipse(180, 200, 17, 30);
 ellipse(220, 200, 17, 30);
-fill(0, 0, 0);
-ellipse(180, 210, 10, 10);
+
+// puil-lines
 fill(255, 255, 255);
 stroke(0, 0, 0);
 strokeWeight(2);
 ellipse(219, 206, 15, 15);
+
+// pupils
 fill(0, 0, 0);
 ellipse(219, 210, 10, 10);
 fill(255, 255, 255);
 ellipse(180, 206, 15, 15);
 fill(0, 0, 0);
 ellipse(180, 210, 10, 10);
+
+// eyebrows
 fill(0, 0, 0);
 rect(170, 180, 15, 4);
 rect(210, 180, 15, 4);
 fill(255, 255, 255);
 noStroke();
 ellipse(190, 230, 20);
+
+// monster hunter layout
+
+// body
+fill(250, 195, 140);
+stroke(130, 69, 67);
+strokeWeight(5);
+ellipse(400, 200, 150);
+
+// eyes
+fill(61, 34, 34);
+noStroke();
+ellipse(400, 200, 25);
+ellipse(470, 200, 25);
