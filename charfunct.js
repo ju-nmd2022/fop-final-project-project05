71aae15ab69af1f35543b36138c6819a0649c65a
mera22qs <118453987+mera22qs@users.noreply.github.com>
2023-05-22 12:57:25 +0200

    Update charfunct.js

--- charfunct.js
+++ charfunct.js
@@ -1,109 +1,121 @@
 //BABY MONSTER
-function baby( ) {
-//body
-fill(238, 161, 221);
-stroke(177, 78, 155);
-strokeWeight(5);
-ellipse(200, 300, 150);
+function baby(x, y) {
+  push();
+  translate(x, y);
 
-//eye
-fill(24, 24, 24);
-noStroke();
-ellipse(200, 295, 60);
+  //body
+  fill(238, 161, 221);
+  stroke(177, 78, 155);
+  strokeWeight(5);
+  ellipse(200, 300, 150);
 
-fill(255, 255, 255);
-noStroke();
-ellipse(210, 285, 20);
+  //eye
+  fill(24, 24, 24);
+  noStroke();
+  ellipse(200, 295, 60);
 
-fill(255, 255, 255);
-noStroke();
-ellipse(190, 303, 25);
+  fill(255, 255, 255);
+  noStroke();
+  ellipse(210, 285, 20);
 
-//blush
+  fill(255, 255, 255);
+  noStroke();
+  ellipse(190, 303, 25);
 
-fill(177, 78, 155);
-ellipse(158, 329, 20, 10);
+  //blush
 
-fill(177, 78, 155);
-ellipse(240, 329, 20, 10);
+  fill(177, 78, 155);
+  ellipse(158, 329, 20, 10);
 
-//pacifier
+  fill(177, 78, 155);
+  ellipse(240, 329, 20, 10);
 
-fill(54, 149, 236);
-noStroke();
-ellipse(196, 342, 15);
+  //pacifier
 
-fill(54, 149, 236);
-noStroke();
-ellipse(206, 342, 15);
+  fill(54, 149, 236);
+  noStroke();
+  ellipse(196, 342, 15);
 
-fill(7, 103, 192);
-noStroke();
-ellipse(201, 342, 15, 5);
+  fill(54, 149, 236);
+  noStroke();
+  ellipse(206, 342, 15);
 
-noFill();
-stroke(7, 103, 192);
-strokeWeight(2);
-bezier(195, 342, 199, 350, 201, 360, 206, 342);
+  fill(7, 103, 192);
+  noStroke();
+  ellipse(201, 342, 15, 5);
 
-//right ear
-push();
-fill(208, 128, 190);
-noStroke();
-translate(158, 0);
-rotate(PI / 8);
-rect(158, 130, 10, 70, 3);
-pop();
+  noFill();
+  stroke(7, 103, 192);
+  strokeWeight(2);
+  bezier(195, 342, 199, 350, 201, 360, 206, 342);
 
-//ear flapp
-push();
-translate(124, -78);
-rotate(PI / 5);
-fill(208, 128, 190);
-noStroke();
-rect(259, 130, 40, 15, 5);
-pop();
+  //right ear
+  push();
+  fill(208, 128, 190);
+  noStroke();
+  translate(158, 0);
+  rotate(PI / 8);
+  rect(158, 130, 10, 70, 3);
+  pop();
 
-//left ear
+  //ear flapp
+  push();
+  translate(124, -78);
+  rotate(PI / 5);
+  fill(208, 128, 190);
+  noStroke();
+  rect(259, 130, 40, 15, 5);
+  pop();
 
-push();
-fill(208, 128, 190);
-noStroke();
-translate(128, 130);
-rotate(PI / -8);
-rect(-10, 54, 10, 70, 3);
-pop();
+  //left ear
 
-//ear flapp
-push();
-fill(208, 128, 190);
-noStroke();
-translate(122, 170);
-rotate(PI / -5);
-rect(-25, 21, 40, 15, 5);
-pop();
+  push();
+  fill(208, 128, 190);
+  noStroke();
+  translate(128, 130);
+  rotate(PI / -8);
+  rect(-10, 54, 10, 70, 3);
+  pop();
 
-//Feet
+  //ear flapp
+  push();
+  fill(208, 128, 190);
+  noStroke();
+  translate(122, 170);
+  rotate(PI / -5);
+  rect(-25, 21, 40, 15, 5);
+  pop();
 
-//right foot
+  //Feet
 
-fill(238, 161, 221);
-stroke(177, 78, 155);
-strokeWeight(5);
-ellipse(170, 378, 40, 10);
+  //right foot
 
-//Left foot
-fill(238, 161, 221);
-stroke(177, 78, 155);
-strokeWeight(5);
-ellipse(230, 378, 40, 10);
+  fill(238, 161, 221);
+  stroke(177, 78, 155);
+  strokeWeight(5);
+  ellipse(170, 378, 40, 10);
+
+  //Left foot
+  fill(238, 161, 221);
+  stroke(177, 78, 155);
+  strokeWeight(5);
+  ellipse(230, 378, 40, 10);
+
+  pop();
 }
 
 // baby monster movement with keys
 
 let x = 100;
 let y = 100;
-let rotation = 3; 
+let speed = 0;
 
+function draw() {
+  baby(x, y);
 
-function draw(); 
+  if (keyIsDown(38)) {
+    speed = 5;
+  } else {
+    speed = 0;
+  }
+}
